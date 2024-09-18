e06d38d7b8d15c5ef94e4618d202b5f4e864f934
Alberto Barrago <albertobarrago@gmail.com>
2024-09-18 12:48:25 +0200

    pylinted ;-)

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+"""
+ Main entry point for the Pong game.
+"""
 from pong_game import PongGame
 
+
 def main():
+    """
+    Main entry point for the Pong game.
+    :return:
+    """
     game = PongGame(game_is_on=True)
     game.run()
+
 
 if __name__ == "__main__":
     main()
